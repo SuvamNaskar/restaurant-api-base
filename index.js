fc69e97229cb5b61ca048ca6c1b6c39deccfd2cf
Suvam Naskar <suvamnaskar780@gmail.com>
2024-10-22 22:07:50 +0530

    MODIFY: tables route and default 'hello world'

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 require("dotenv").config();
-const memberRouter = require('./routes/member')
+const memberRouter = require('./routes/member');
+const tableRouter = require('./routes/table');
 
 const app = express();
 const port = 3000;
 
 // Middleware
 app.use(express.json());
-
-// Middleware
 app.use('/members', memberRouter);
+app.use('/tables', tableRouter);
 
-
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>');
+});
 
 // Start server
 app.listen(port, () => {
