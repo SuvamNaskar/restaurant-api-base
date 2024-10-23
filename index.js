2b87911240ba90afd574425303d622b37d6e1576
Suvam Naskar <suvamnaskar780@gmail.com>
2024-10-23 19:00:49 +0530

    MODIFY: basicAuth middleware added

--- index.js
+++ index.js
@@ -2,20 +2,21 @@ const express = require('express');
 require("dotenv").config();
 const memberRouter = require('./routes/member');
 const tableRouter = require('./routes/table');
+const basicAuth = require('./middleware/basicAuth');
 
 const app = express();
 const port = 3000;
 
 // Middleware
 app.use(express.json());
-app.use('/members', memberRouter);
-app.use('/tables', tableRouter);
+app.use('/members', basicAuth, memberRouter);
+app.use('/tables', basicAuth, tableRouter);
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>');
+	res.send('<h1>Hello World!</h1>');
 });
 
 // Start server
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+	console.log(`Server is running on http://localhost:${port}`);
 });
